4bbed64fcb83a7f4cc5347200a0fdb5f444dffc4
Sinan Dogan <sinnandogann@outlook.com>
2023-02-25 13:44:41 +0300

    first commit

---
+++ todo-project/src/App.vue
@@ -0,0 +1,127 @@
+<script setup>
+import {ref, onMounted, computed, watch} from 'vue'
+
+//Yapılacaklar bu boş diziye listelencektir.
+const todos=ref([])
+const name =ref('')
+
+const input_content =ref('')
+const input_category =ref(null)
+
+//eklenen verilerin tarihe göre ters sıralanmış hali
+const todos_asc=computed(()=>todos.value.sort((a,b)=>{
+  return b.createdAt-a.createdAt
+}))
+
+
+const removeTodo =todo =>{
+  todos.value=todos.value.filter(t=>t !== todo)
+}
+
+const addTodo=()=>{
+
+  if(input_content.value.trim() === '' || input_category.value === null){
+    return
+  }
+   
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done:false,
+    createdAt: new Date().getTime()
+  })
+
+  input_content.value=''
+  input_category.value=null
+
+}
+
+//"todos" adlı değişken izleniyor ve herhangi bir değişiklik olduğunda "newWal" adlı yeni değer alınarak localStorage'a kaydediliyor.
+//"todos" adlı  nesnenin herhangi bir özelliği değişirse, "deep: true" özelliği kullanılarak bu değişiklik izlenebilir. 
+//Eğer "deep: true" özelliği kullanılmazsa, sadece "todos" nesnesinin kendisi izlenir ve içindeki özelliklerin değişiklikleri takip edilmez.
+watch(todos, newWal=>{
+  localStorage.setItem('todos',JSON.stringify(newWal))
+}, {deep:true})
+
+//isim parametresi değiştiğinde localstorage içerisinde bulunun isim yeni değerini dinamik olarakalacaktır.
+watch(name,(newVal) =>{
+  localStorage.setItem('name',newVal)
+})
+
+
+//Burdaki kod localstorage'de bir name veya todos var ise onu sayfa yüklenince gösterir eğer yoksa boş bir dize atar.
+onMounted(()=>{
+  name.value=localStorage.getItem('name') || ''
+  todos.value=JSON.parse(localStorage.getItem('todos')) || []
+})
+</script>
+
+<template>
+   <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        Whats's up, <input type="text" placeholder="Name here" v-model="name"/>
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+      <h4>What's on your todo list?</h4>
+      <input type="text" placeholder="e.g. make a project" v-model="input_content">
+      <h4>Pick a category</h4>
+      <div class="options">
+        <label>
+          <input type="radio"
+           name="category" 
+            value="business" 
+            v-model="input_category"/>
+            <span class="bubble business"></span>
+            <div>Business</div>
+        </label>
+
+        <label>
+          <input type="radio"
+           name="category" 
+            value="personal" 
+            v-model="input_category"/>
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+        </label>
+      </div>
+
+       <input type="submit" value="Add todo">
+
+      </form>
+
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="` todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done"/>
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          
+          <div class="todo-content">
+            <input type="text" v-model="todo.content"/>
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+   </main>
+</template>
+
+
